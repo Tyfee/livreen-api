32ab0639e45cd9e5fbc13af9e11553ab5420235d
Tyfee <121516618+Tyfee@users.noreply.github.com>
2023-06-30 11:20:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 import requests
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+origins = [
+    "http://livreen-tyfee.vercel.app",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get('/')
 async def root():
